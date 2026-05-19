5d812ffbc29c153dfa5c23a3fabe216701256610
Dani <eiben.hauptfach0a@icloud.com>
2026-05-19 22:13:42 +0200

    Confirm mobile kassa payments and fix popup positioning

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'drink-pos-shell-v72';
+const CACHE_NAME = 'drink-pos-shell-v73';
 const SHELL_URLS = ['/', '/admin', '/kassa', '/manifest.webmanifest', '/icon.svg', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', event => {
